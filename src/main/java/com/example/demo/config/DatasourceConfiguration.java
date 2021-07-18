ce238e52914574e8be9d6905a9883ad074d9a4df
Ashok Vardhan <bashokvardhan@gmail.com>
2021-07-18 17:55:30 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/config/DatasourceConfiguration.java
@@ -0,0 +1,36 @@
+package com.example.demo.config;
+
+import com.example.demo.config.properties.DataSourceProperties;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import oracle.jdbc.pool.OracleDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class DatasourceConfiguration {
+    private HikariConfig hikariConfig;
+    private DataSourceProperties dataSourceProperties;
+
+    public DatasourceConfiguration(HikariConfig hikariConfig, DataSourceProperties dataSourceProperties) {
+        this.hikariConfig = hikariConfig;
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        OracleDataSource dataSource = new OracleDataSource();
+        dataSource.setUser(dataSourceProperties.getUserName());
+        dataSource.setPassword(dataSourceProperties.getPassword());
+        dataSource.setURL(dataSourceProperties.getUrl());
+        dataSource.setImplicitCachingEnabled(true);
+        hikariConfig.setDataSource(dataSource);
+        hikariConfig.setDriverClassName(dataSourceProperties.getDriverClassName());
+        hikariConfig.setConnectionTestQuery(dataSourceProperties.getConnectionTestQuery());
+        hikariConfig.setMinimumIdle(dataSourceProperties.getMinimumIdle());
+        hikariConfig.setMaximumPoolSize(dataSourceProperties.getMaxPoolSize());
+        return new HikariDataSource(hikariConfig);
+    }
+}
---
+++ src/main/java/com/example/demo/config/properties/DataSourceProperties.java
@@ -0,0 +1,70 @@
+package com.example.demo.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("loan-defaults-datasource")
+public class DataSourceProperties {
+    private String url;
+    private String userName;
+    private String password;
+    private String driverClassName;
+    private int maxPoolSize;
+    private int minimumIdle;
+    private String connectionTestQuery;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public int getMaxPoolSize() {
+        return maxPoolSize;
+    }
+
+    public void setMaxPoolSize(int maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    public int getMinimumIdle() {
+        return minimumIdle;
+    }
+
+    public void setMinimumIdle(int minimumIdle) {
+        this.minimumIdle = minimumIdle;
+    }
+
+    public String getConnectionTestQuery() {
+        return connectionTestQuery;
+    }
+
+    public void setConnectionTestQuery(String connectionTestQuery) {
+        this.connectionTestQuery = connectionTestQuery;
+    }
+}
